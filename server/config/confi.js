72b453fc9e13191b3bffc9eb621d8c010cc4614b
Jose pinzon <jmpinzon90@misena.edu.co>
2019-01-16 17:14:02 -0500

    Restserver - usuario

--- server/config/confi.js
+++ server/config/confi.js
@@ -7,6 +7,6 @@ let url;
 if (process.env.NODE_ENV === 'dev') {
     url = 'mongodb://localhost:27017/cafe';
 } else {
-    url = 'mongodb://act:torrent123@ds151180.mlab.com:51180/cafe';
+    url = 'mongodb://usuario:contraseña@ds151180.mlab.com:51180/basededatos';
 }
 process.env.URLDB = url;
